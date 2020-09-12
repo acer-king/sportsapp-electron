f0b501d546d48540a7139e3be401c9e2a74d6a53
acerking03 <acerking03@gmail.com>
2020-09-12 20:13:50 +0800

    add position

--- app/static/js/sportsoptimizer.js
+++ app/static/js/sportsoptimizer.js
@@ -13,6 +13,14 @@
 
 	var win = remote.getCurrentWindow();
 
+	$.each(['show', 'hide'], function (i, ev) {
+		var el = $.fn[ev];
+		$.fn[ev] = function () {
+			this.trigger(ev);
+			return el.apply(this, arguments);
+		};
+	});
+
 	$('#minimize').click(function () {
 		win.minimize();
 	});
@@ -39,7 +47,8 @@
 		$("#optimizer-builder").steps({
 			headerTag: "h3",
 			bodyTag: "section",
-			stepsOrientation: "vertical"
+			stepsOrientation: "vertical",
+			transitionEffect: "slideLeft"
 		});
 	}
 
@@ -52,18 +61,39 @@
 	})
 
 	var g_index = 0;
-	$("#Bt_Add_Position").click(function(){
-		var rowID= "row" + g_index.toString();
+	$("#Bt_Add_Position").click(function () {
+		var rowID = "row" + g_index.toString();
 		var deleteId = "delete_" + g_index.toString();
 		g_index += 1;
-		$("#positionsCreated").append("<tr id=\"" + rowID + "\">	<td><input id=\"CreatePositionValue\" type=\"tel\" value=\"\" placeholder=\"Enter position/s... Ex. C or C,1B,2B for flex/util\"></td>		<td class=\"right\"><i id=\""+ deleteId +"\" class=\""+ "ic-delete-1" + "\" aria-hidden=\"true\"></i></td></tr>");
+		$("#positionsCreated").append("<tr id=\"" + rowID + "\">	<td><input id=\"CreatePositionValue\" type=\"tel\" value=\"\" placeholder=\"Enter position/s... Ex. C or C,1B,2B for flex/util\"></td>		<td class=\"right\"><i id=\"" + deleteId + "\" class=\"" + "ic-delete-1" + "\" aria-hidden=\"true\"></i></td></tr>");
 	})
-	$("#positionsCreated").on('click','.ic-delete-1',function(){
+
+	$("#positionsCreated").on('click', '.ic-delete-1', function () {
 		var id = this.id;
 		var split_id = id.split("_");
 		var deleteIdx = split_id[1];
 		//remove tr with this idx
-		$("#row"+deleteIdx).remove();
+		$("#row" + deleteIdx).remove();
+	})
+
+	$("#RulesIdInCreating").mouseenter(function () {
+		
+		$('#rules_noopponent_creation').empty().append('<option value="">None</option>');
+		var dataList = $('#CreatePositionValue').map(function(){
+			console.log($(this).val())
+			return $(this).val();
+		}).get().join(',');
+		
+		var dataList = dataList.split(',');
+		dataList.map(function(item){
+			item = item.trim();
+			console.log(item);
+			$('#rules_noopponent_creation').append(new Option(item,"value"));
+		})
 	})
 
 })(jQuery);
+
+
+
+
